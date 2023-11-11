a3f7d7e0feab91511f49f0b5eec410b5f2153f1e
siampathan005@gmail.com <siampathan005@gmail.com>
2023-11-12 02:17:14 +0600

    remove some comment

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   const [data, setData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  //const API_KEY = "e70528456668973f6dcf59191436f5f2";
 
   useEffect(() => {
     fetch(`https://restcountries.com/v3.1/name/${countryName}`)
@@ -75,35 +74,6 @@ function App() {
               </div>
             </div>
           </div>
-
-          {/* <div className="group">
-            <img
-              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-              alt=""
-            />
-            <h1>{data.weather[0].main}</h1>
-          </div>
-
-          <h1 className="temp">{data.main.temp.toFixed()} °C</h1>
-
-          <Slide direction="right" timeout={800} in={!loading}>
-            <div className="box_container">
-              <div className="box">
-                <p>Humidity</p>
-                <h1>{data.main.humidity.toFixed()}%</h1>
-              </div>
-
-              <div className="box">
-                <p>Wind</p>
-                <h1>{data.wind.speed.toFixed()} km/h</h1>
-              </div>
-
-              <div className="box">
-                <p>Feels Like</p>
-                <h1>{data.main.feels_like.toFixed()} °C</h1>
-              </div>
-            </div>
-          </Slide> */}
         </div>
       ) : (
         <h2>Loading</h2>
